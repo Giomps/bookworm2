3d0f81009c92b09d40802f4735682ab819e68768
“GigGit” <front@fedhubs.fr>
2023-03-10 00:40:37 +0100

    10/03/2023

--- index.js
+++ index.js
@@ -34,40 +34,43 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
+
 /* Page d'accueil  */
 app.get('/', async function(req, res, next) {
+
+    let products = await productsModel.find()
+
     /* Initier la session du panier */
     if (req.session.basket == undefined) {
         req.session.basket = []
     }
-
     if (req.session.user) {
-        res.render('menu', {user: req.session.user.user})
+        res.render('menu', {products, item: req.session.basket.length, user: req.session.user.user})
     } 
 
-    console.log(req.session.user)
-
-    let products = await productsModel.find()
-    res.render('menu', {userOne: req.session.user, products, item: req.session.basket.length})
+    res.render('menu', {user: req.session.user, products, item: req.session.basket.length})
 })
 
 /* Afficher un produit en détail */
 app.get('/article/:id', async  function(req, res) {
     let productId = req.params.id
     let product = await productsModel.findById(productId);
-    res.render('article', {product, item: req.session.basket.length})
+    res.render('article', {user: req.session.user, product, item: req.session.basket.length})
 })
 
 /* Afficher le panier */
-app.get('/panier', function(req, res) {
-    res.render('panier', {basket: req.session.basket, item: req.session.basket.length})
+app.get('/panier', async function(req, res) {
+    let products = await productsModel.find()
+    res.render('panier', {products, user: req.session.user, basket: req.session.basket, item: req.session.basket.length})
 })
 
 /* Ajouter un produit dans le panier  */
-app.post('/add', function(req, res) {
+app.post('/add', async function(req, res) {
+    let products = await productsModel.find()
     req.session.basket.push(products[req.body.position])
     res.render('menu', {products, item: req.session.basket.length})
 })
+
 
 /* Afficher la page de connexion */
 app.get('/connexion',  function(req, res) {
@@ -90,7 +93,10 @@ app.post('/connexion', async function(req, res) {
     }
     
     if(user && passwordMatch) {
-        req.session.user = {user: user}
+        req.session.user = {
+            prenom: user.prenom,
+            id: user._id,
+        }
         return res.redirect('/')
     } 
 
@@ -103,7 +109,7 @@ app.get('/inscription', function(req, res) {
 })
 
 /* Envoyer le formulaire à la base de données */
-app.post('/send', async function(req, res, next) {
+app.post('/send', async function(req, res) {
     /* Recuperer un email existant sur une base de données  */
     let alreadyExist = await clientModel.findOne({email: req.body.email})
     /* Générer le cryptage des mots de passe */
@@ -112,14 +118,17 @@ app.post('/send', async function(req, res, next) {
     /*  Initialisation du message d'erreur  */
     let message = []
 
+    /* Si l'email est déjà existant, alors on affiche un message d'erreur */
     if (alreadyExist) {
         message.push("L'email est déjà existant. Veuillez en choisir un autre")
     }
-
+     /* Si les deux mots de passe ne sont pas identiques, alors on affiche un message d'erreur */
     if (req.body.password != req.body.password_second) {
         message.push("les mots de passe ne sont pas identiques")
     }
 
+    /* Si l'email n'est pas encore inscrit dans la base de données et que les deux mots de passe sont identiques, 
+       alors on envoie le formulaire dans la base de données  */
     if (alreadyExist != req.body.email && req.body.password == req.body.password_second) {
         let newClient = new clientModel({
             nom: req.body.nom,
@@ -129,8 +138,15 @@ app.post('/send', async function(req, res, next) {
             address: req.body.address,
             password: hash
         })
+        /* On stocke le modèle de l'inscription dans une variable et avec la variable, 
+           on affecte le id et prenom du modèle dans la session user */ 
         let client = await newClient.save()
-        return res.redirect('/connexion')
+        req.session.user = {
+            prenom: client.prenom,
+            id: client._id
+        }
+        /* On redirige vers la page du menu  */
+        return res.redirect('/')
     }
 
     res.render('inscription', {message, item:req.session.basket})
@@ -138,18 +154,13 @@ app.post('/send', async function(req, res, next) {
 
             /*  Client */
 app.get('/monCompte', function(req,res) {
-    res.render('monCompte', {userOne: req.session.user.length,item:req.session.basket.length})
+    res.render('monCompte', {user: req.session.user ,item:req.session.basket.length})
 })
 
 /* Mettre fin à la session AKA se deconnecter */
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect('/');
-      }
-    });
+app.get('/logout', function(req, res) {
+    req.session.user = null
+    res.redirect('/')
 });
 
 /* ERROR 404 */
---
+++ models/panier.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+let panierSchema = mongoose.Schema({
+    
+})
